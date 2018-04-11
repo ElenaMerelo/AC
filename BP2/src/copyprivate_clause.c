0cc664f744c4095fadefb0fe0dbe746c9e3f6875
elenamerelo <nenamer@gmail.com>
2018-04-11 18:49:08 +0200

    Ejers seminario done, añadidos conocimientos a openMP.md

---
+++ BP2/src/copyprivate_clause.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+  int n = 9, i, b[n];
+  for (i=0; i<n; i++)
+    b[i] = -5;
+
+  #pragma omp parallel
+  {
+    int a;
+    #pragma omp single
+    {
+      printf("\nIntroduce valor de inicialización a: ");
+      scanf("%d", &a );
+      printf("\nSingle ejecutada por el thread %d\n", omp_get_thread_num());
+    }
+
+    #pragma omp for
+      for (i=0; i<n; i++) b[i] = a;
+  }
+
+  printf("Después de la región parallel:\n");
+  for (i=0; i<n; i++)
+    printf("b[%d] = %d\t",i,b[i]);
+  printf("\n");
+}
